6f5efc7b9b322c35bb2f17b0f785dce7ffb5f11b
SURAJ KUMAR <skumar27@skumar274MD6M.vmware.com>
2023-08-23 11:18:33 +0530

    Implement basic workflow with mongoDb used

--- nsx-alb-tools-angular-app/src/app/app.component.ts
+++ nsx-alb-tools-angular-app/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+import { finalize } from 'rxjs/operators';
+import { dataToInsert } from './app.constants';
 
 @Component({
   selector: 'app-root',
@@ -9,21 +12,79 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
   title = 'NSX Advanced Load Balancer Tools';
 
-  public responseMessage: string = '';
+  public responseMessage: any;
+
+  private isLoading = new BehaviorSubject<boolean>(false);
 
   constructor(private http: HttpClient) { }
 
-  public handleClick() {
-    this.http.get('/button-click').subscribe(
-      (data) => {
+  public handleGetData() {
+    this.showLoading();
+
+    this.responseMessage = undefined;
+
+    this.http.get('/migrationTools/getData').pipe(
+      finalize(() => this.hideLoading())).subscribe({
+      next: (data) => {
         // This callback will be triggered for both successful responses and 304 responses.
         console.log('Data received:', data);
-        this.responseMessage = data as string;
+        this.responseMessage = data;
       },
-      (error) => {
+      error: (error) => {
         // This callback will not be triggered for a 304 response.
         console.error('Error:', error);
       }
-    );
+    });
+  }
+
+  public handleClearData() {
+    this.showLoading();
+
+    this.responseMessage = undefined;
+
+    this.http.delete('/migrationTools/clearData').pipe(
+      finalize(() => this.hideLoading())).subscribe({
+        next: (data:any) => {
+          // This callback will be triggered for both successful responses and 304 responses.
+          console.log('Cleared Data:', data);
+          this.responseMessage = data['message'];
+        },
+        error: (error) => {
+          // This callback will not be triggered for a 304 response.
+          console.error('Error:', error);
+        }
+      });
+  }
+
+  public async handleStorerData() {
+    this.showLoading();
+    await this.handleClearData();
+
+    this.responseMessage = undefined;
+
+    this.http.post('/migrationTools/storeData', dataToInsert).pipe(
+      finalize(() => this.hideLoading())).subscribe({
+        next: (data: any) => {
+          // This callback will be triggered for both successful responses and 304 responses.
+          console.log('stored Data:', data);
+          this.responseMessage = data.message;
+        },
+        error: (error) => {
+          // This callback will not be triggered for a 304 response.
+          console.error('Error:', error);
+        }
+      });
+  }
+
+  public showLoading() {
+    this.isLoading.next(true);
+  }
+
+  public get loading$() {
+    return this.isLoading.asObservable();
+  }
+
+  public hideLoading() {
+    this.isLoading.next(false);
   }
 }
