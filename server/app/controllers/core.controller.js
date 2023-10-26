9ad87f2bb3d7cf894ef53f3294bafd7961bf72bd
Vimal Gohil <vgohil@vmware.com>
2023-10-26 15:41:33 +0530

    AV-191864: Add changes to get Avi Lab details and Avi Destination details with Mappings

--- server/app/controllers/core.controller.js
+++ server/app/controllers/core.controller.js
@@ -1,5 +1,9 @@
 const asyncHandler = require("express-async-handler");
 const mockData = require("../../data/mock/f5destination.json");
+const {
+    AviLabDetailsModel, 
+    AviDestinationDetailsModel,
+} = require('../models/migration.model');
 
 exports.getAviDestinationMappings = asyncHandler(async (req, res, next) => {
     // TODO: Parse the destination mappings in required format once it is available.
@@ -19,5 +23,28 @@ exports.getAviDestinationMappings = asyncHandler(async (req, res, next) => {
         res.status(400).json({ error: 'Missing required fields.' });
     } else {
         res.status(200).json(mockData);
+    }
+});
+
+exports.getAviLabDetails = asyncHandler(async (req, res, next) => {
+    try {
+        const labDetails = await AviLabDetailsModel.find({});
+        console.log(labDetails);
+
+        res.status(200).json(labDetails);
+    } catch (err) {
+        res.status(500).json({ message: 'Error in fetching the Avi Lab controller details, '+err.message});
+    }
+     
+});
+
+exports.getAviDestinationDetails = asyncHandler(async (req, res, next) => {
+    try {
+        const destinationDetails = await AviDestinationDetailsModel.find({});
+        console.log(destinationDetails);
+
+        res.status(200).json(destinationDetails);
+    } catch (err) {
+        res.status(500).json({ message: 'Error in fetching the Avi Destination controller & Mappings details, '+err.message});  
     }
 });
