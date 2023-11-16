3b9e236621a59b0bc1ffc6a4edf970bdd82b8e1f
SURAJ KUMAR <skumar27@skumar274MD6M.vmware.com>
2023-11-16 12:28:36 +0530

    Error handling for configuration section

--- nsx-alb-tools-angular-app/src/app/migration-tools/f5/f5-configuration/f5-configuration.component.ts
+++ nsx-alb-tools-angular-app/src/app/migration-tools/f5/f5-configuration/f5-configuration.component.ts
@@ -1,5 +1,6 @@
 import {
   Component,
+  OnDestroy,
   OnInit
 } from '@angular/core';
 
@@ -21,12 +22,16 @@ const { ENGLISH: dictionary } = l10n;
   templateUrl: './f5-configuration.component.html',
   styleUrls: ['./f5-configuration.component.less'],
 })
-export class F5ConfigurationComponent implements OnInit {
+export class F5ConfigurationComponent implements OnInit, OnDestroy {
   public incompleteVSMigrationsData: incompleteVsMigration[] = [];
 
   public selectedMigrationIndex = 0;
 
   public completedVSMigrationsCount = 0;
+
+  public hasError = false;
+
+  public isLoadingMigrationsData = false;
 
   public isOpenVsConfigEditorModal = false;
 
@@ -43,12 +48,22 @@ export class F5ConfigurationComponent implements OnInit {
 
   /** @override */
   public async ngOnInit(): Promise<void> {
-    await this.getAllIncompleteVSMigrationsData();
-    await this.getMigrationOverviewData();
+      await this.getAllIncompleteVSMigrationsData();
+      await this.getMigrationOverviewData();
   }
 
-  public handleCloseVsConfigEditor(): void {
+  public handleCloseVsConfigEditor(isConfigurationAccepted: boolean): void {
+    if (isConfigurationAccepted) {
+      this.removeVsMigrationData(this.selectedMigrationIndex);
+    }
+
     this.isOpenVsConfigEditorModal = false;
+  }
+
+  public removeVsMigrationData(selectedMigrationIndex: number): void {
+    if (selectedMigrationIndex !== -1) {
+      this.incompleteVSMigrationsData.splice(selectedMigrationIndex, 1);
+    }
   }
 
   public handleSkip(): void {
@@ -59,32 +74,54 @@ export class F5ConfigurationComponent implements OnInit {
     this.isOpenVsConfigEditorModal = true;
   }
 
-  public async getMigrationOverviewData(): Promise<void> {
+  public async handleFetch(): Promise<void> {
+    try {
+      this.isLoadingMigrationsData = true;
+
+      const fetchFromController$ = this.configurationTabService.fetchFromController();
+
+      await lastValueFrom(fetchFromController$);
+      await this.getAllIncompleteVSMigrationsData();
+
+      this.configurationTabService.showCompletedMigrationsCountAlert = true;
+    } catch (error) {
+      this.hasError = true;
+    } finally {
+      this.isLoadingMigrationsData = false;
+    }
+  }
+
+  public onErrorAlertClose(): void {
+    this.hasError = false;
+  }
+
+  public onSuccessAlertClose(): void {
+    this.configurationTabService.showCompletedMigrationsCountAlert = false;
+  }
+
+  /** * @override */
+  public ngOnDestroy(): void {
+    this.configurationTabService.showCompletedMigrationsCountAlert = false;
+  }
+
+  private async getMigrationOverviewData(): Promise<void> {
     const migrationOverviewData$ = this.configurationTabService.getMigrationOverviewData();
     this.migrationOverviewData = await lastValueFrom(migrationOverviewData$);
   }
 
-  public async handleLabControllerCardFetch(): Promise<void> {
-    this.incompleteVSMigrationsData = [];
-    this.completedVSMigrationsCount = 0;
-
-    const fetchFromController$ = this.configurationTabService.fetchFromController();
-    await lastValueFrom(fetchFromController$);
-
-    await this.getAllIncompleteVSMigrationsData();
-
-    this.configurationTabService.showCompletedMigrationsCountAlert = true;
-  }
-
   private async getAllIncompleteVSMigrationsData(): Promise<void> {
-    const data$ = this.configurationTabService.getAllIncompleteVSMigrationsData();
-    const data: incompleteVsMigrationsData = await lastValueFrom(data$);
+    try {
+      this.isLoadingMigrationsData = true;
 
-    this.incompleteVSMigrationsData = data.incompleteVSMigrationsData;
-    this.completedVSMigrationsCount = data.completedVSMigrationsCount;
-  }
+      const data$ = this.configurationTabService.getAllIncompleteVSMigrationsData();
+      const data: incompleteVsMigrationsData = await lastValueFrom(data$);
 
-  public onAlertClose(): void {
-    this.configurationTabService.showCompletedMigrationsCountAlert = false;
+      this.incompleteVSMigrationsData = data.incompleteVSMigrationsData || [];
+      this.completedVSMigrationsCount = data.completedVSMigrationsCount || 0;
+    } catch (error) {
+      this.hasError = true;
+    } finally {
+      this.isLoadingMigrationsData = false;
+    }
   }
 }
