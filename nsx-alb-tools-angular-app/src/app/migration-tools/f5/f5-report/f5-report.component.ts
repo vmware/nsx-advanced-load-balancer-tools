315ff94f12c26746a546b23885ef4fefa3536d7e
Vimal Gohil <vgohil@vmware.com>
2023-11-21 15:08:06 +0530

    AV-193480: Adding changes to fix the donut chart issues

--- nsx-alb-tools-angular-app/src/app/migration-tools/f5/f5-report/f5-report.component.ts
+++ nsx-alb-tools-angular-app/src/app/migration-tools/f5/f5-report/f5-report.component.ts
@@ -12,12 +12,12 @@ import * as d3 from 'd3';
 const { ENGLISH: dictionary, ...l10nKeys } = l10n;
 
 let colors = [
-  "#225677",
-  "#C9E0ED",
-  "#945DB7",
-  "#737373",
-  "#454545",
-  "#B0D3E5",
+  '#225677',
+  '#C9E0ED',
+  '#945DB7',
+  '#737373',
+  '#854545',
+  '#B0D3E5',
 ]
 @Component({
   selector: 'f5-report',
@@ -35,8 +35,8 @@ export class F5ReportComponent implements OnInit {
     public openStartMigrationWizard: boolean = false;
 
     private margin = { top: 10, right: 10, bottom: 10, left: 10 };
-    private width = 450;
-    private height = 250;
+    private width = 420; // 500;
+    private height = 230; // 270;
     private svg: any;
     private colors: any = colors;
     private radius = Math.min(this.width, this.height) / 2 - this.margin.left;
@@ -50,11 +50,11 @@ export class F5ReportComponent implements OnInit {
       this.http.get('discovery/getReport').subscribe((data)=> {
         this.report = data;
         const chart = [];
-        const keys = Object.keys(this.report.virtualService.types);
+        const keys = Object.keys(this.report.virtualServices.types);
         keys.forEach((key, index) => {
           const each = {
             name: key,
-            value: this.report.virtualService.types[key],
+            value: this.report.virtualServices.types[key],
             color: this.colors[index],
           };
           chart.push(each as never);
@@ -67,13 +67,13 @@ export class F5ReportComponent implements OnInit {
 
     private createSvg(): void {
       this.svg = this.d3.d3
-        .select("figure#donut")
+        .select("div#donut")
         .append("svg")
         .attr("viewBox", `0 0 ${this.width} ${this.height}`)
         .append("g")
         .attr(
           "transform",
-          "translate(" + this.width / 4 + "," + this.height / 2 + ")"
+          "translate(" + this.width / 3 + "," + this.height / 2 + ")"
         );
     }
 
@@ -96,6 +96,7 @@ export class F5ReportComponent implements OnInit {
           index++;
         }
       });
+      
       this.colors = this.d3.d3
         .scaleOrdinal()
         .domain(data.map((d: any) => d.value.toString()))
@@ -125,42 +126,63 @@ export class F5ReportComponent implements OnInit {
         .enter()
         .append("path")
         .attr("d", arc)
-        .attr("fill", (d: any) => this.colors(d.data.value))
+        .attr("fill", (d: any) => d.data.color)
         .attr("stroke", "white")
         .style("stroke-width", "2px")
-        .style("opacity", 0.7);
+        .style("opacity", 0.7)
+        // .on("mouseover", function (event, d) {
+        //     const tooltip = d3.select("#tooltip");
 
-        var legendItemSize = 20;
-        var legendSpacing = 4;
+        //     tooltip
+        //       .select('#value')
+        //       .text(d.data.name + ' - ' + d.data.value + '%')
 
-        var legend = this.svg
-          .selectAll('.legend')
+        //       console.log(event);
+
+        //     tooltip
+        //     .style("left", (event.pageX) + "px")
+        //     .style("top", (event.pageY) + "px")
+        //       //.style("left", (event.pageX-280) + "px")
+        //       //.style("top", (event.pageY-340) + "px")
+        //       .style("opacity", 1)
+        // })
+        // .on("mouseout", function () {
+        //   // Hide the tooltip
+        //     d3.select("#tooltip")
+        //     .style("opacity", 0);
+        // });
+
+        let legends = d3.select('#legendContainer')
+          .selectAll('.legend-row')
           .data(data)
           .enter()
-          .append('g')
-          .attr('class', 'legend')
-          .attr('transform', (d, i) => {
-            var height = legendItemSize + legendSpacing;
-            var offset = data.length / 2;
-            var x = legendItemSize * 10;
-            var y = (i * height) - (offset*25);
-            return `translate(${x}, ${y})`
-          })
+          .append('div')
+          .attr('style', 'display: flex; margin-bottom: 4px;');
+        
+        legends
+          .append('div')
+          .attr('style', (d: any) => {
+            return `content: ''; 
+              width: 15px; 
+              height: 15px; 
+              border-raidus: 7.5px; 
+              -moz-border-radius: 7.5px; 
+              -webkit-border-radius: 7.5px; 
+              background-color: ${d.color}`;
+          });
+          
+        let legendData = legends
+          .append('div')
+          .attr('style', 'display: flex; justify-content: space-between; margin-left: 10px; margin-top: -3px; width: 80px;');
 
-        legend
-          .append('circle')
-          .attr('cx', legendItemSize/10)
-          .attr('cy', legendItemSize/2)
-          .attr("r", 10)
-          .style('fill', this.colors);
+        legendData  
+          .append('div')
+          .text((d: any) => d.name);
 
-        legend
-          .append('text')
-          .attr('x', legendItemSize + legendSpacing)
-          .attr('y', legendItemSize - legendSpacing)
-          .text((d) => {
-            return d.name + ' ' + d.value;
-          })
+        legendData
+          .append('div')
+          .text((d: any) => d.value);
+
     }
 
     public handleOpenStartMigrationWizard(): void {
