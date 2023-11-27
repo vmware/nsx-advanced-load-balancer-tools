63e6b73b4f957481887cb4879c33052e64c3dc0d
Abhinesh Gour <agour@vmware.com>
2023-11-27 16:30:02 +0530

    AV-193307: Updated error code and added default values for error scenarios

--- nsx-alb-tools-angular-app/src/app/migration-tools/f5/f5-ready/f5-ready.l10n.ts
+++ nsx-alb-tools-angular-app/src/app/migration-tools/f5/f5-ready/f5-ready.l10n.ts
@@ -4,7 +4,7 @@
  */
 
 export const ENGLISH = {
-  genericErrorMessage: 'Error while fetching the data. Try again by refreshing the page.',
+  genericErrorMessage: 'Error while fetching the data.',
   genericModalErrorMessage: 'Something went wrong. Please try again after some time.',
   readyPageDestinationControllerTitle: 'Destination Controller',
   readyPageEditDestinationCtrlTitle: 'Edit Destination Controller Details',
